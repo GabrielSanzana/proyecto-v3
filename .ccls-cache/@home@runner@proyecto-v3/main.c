b6b36f4d927fceca70aea70774bc91be6d0cf865
lRaging <127540478+lRaging@users.noreply.github.com>
2023-06-28 03:46:14 +0000

    tda

--- .ccls-cache/@home@runner@proyecto-v3/main.c
+++ .ccls-cache/@home@runner@proyecto-v3/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "hashmap.h"
+#include "list.h"
+#include "heap.h"
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+typedef struct{
+  char* nombre;
+  int precioCompra;
+  int precioVenta;
+  int stockInicial;
+  int cantVendida;
+}tipoProducto;
+
+typedef struct{
+  int gananciaDia;
+  int gastoDia;
+}tipoDia;
+
+int main()
+{
+  
 }
