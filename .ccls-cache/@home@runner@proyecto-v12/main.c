7897cbcb1fac0d2abe09b53bb6434be49e998206
lRaging <127540478+lRaging@users.noreply.github.com>
2023-06-30 03:47:38 +0000

    version final 0.1

---
+++ .ccls-cache/@home@runner@proyecto-v12/main.c
@@ -0,0 +1,377 @@
+#include "hashmap.h"
+#include "heap.h"
+#include "list.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+  char *nombre;
+  int precioCompra;
+  int precioVenta;
+  int stockInicial;
+  int cantVendida;
+} tipoProducto;
+
+typedef struct {
+  int gananciaDia;
+  int gastoDia;
+} tipoDia;
+
+void registro_de_productos(HashMap *mapaProducto, HashMap *mapaSemanal, tipoDia *dia) {
+  char *nombre;
+  int precioCompra, precioVenta, stockInicial, cantVendida;
+  printf("Ingrese el nombre del producto, precio de compra, precio de venta y "
+         "stock. Separado por comas\n");
+  scanf("%m[^,],%d,%d,%d", &nombre, &precioCompra, &precioVenta, &stockInicial);
+  tipoProducto *productoAux = malloc(sizeof(tipoProducto));
+  productoAux->nombre = strdup(nombre);
+  productoAux->precioCompra = precioCompra;
+  productoAux->precioVenta = precioVenta;
+  productoAux->stockInicial = stockInicial;
+  productoAux->cantVendida = 0;
+  dia->gastoDia += stockInicial * precioCompra;
+  tipoProducto *copiaProductoAux = malloc(sizeof(tipoProducto));
+  copiaProductoAux->nombre = strdup(nombre);
+  copiaProductoAux->precioCompra = precioCompra;
+  copiaProductoAux->precioVenta = precioVenta;
+  copiaProductoAux->stockInicial = stockInicial;
+  copiaProductoAux->cantVendida = 0;
+
+  insertMap(mapaProducto, productoAux->nombre, productoAux);
+  insertMap(mapaSemanal, copiaProductoAux->nombre, copiaProductoAux);
+}
+
+void Control_de_stock(HashMap *mapaProducto, HashMap *mapaSemanal, tipoDia *dia) {
+  // pensar en poner variables que sumen la ganancia e inversion total
+  char *nombre;
+  int cantVendida;
+  printf("Ingrese el nombre del producto y las unidades que vendio. Separado "
+         "por una coma\n");
+  scanf("%m[^,],%d", &nombre, &cantVendida);
+  Pair *buscado = searchMap(mapaProducto, nombre);
+  if (buscado == NULL) {
+    printf("El producto no existe.");
+    return;
+  }
+  tipoProducto *productoAux = buscado->value;
+  tipoProducto *productoEnSemanal = searchMap(mapaSemanal, nombre)->value;
+  if (productoAux->cantVendida + cantVendida > productoAux->stockInicial ||
+      cantVendida <= 0) {
+    puts("No se puede.");
+    return;
+  }
+  productoAux->cantVendida = productoAux->cantVendida + cantVendida;
+  dia->gananciaDia = cantVendida * (productoAux->precioVenta-productoAux->precioCompra);
+  productoEnSemanal->cantVendida = productoEnSemanal->cantVendida + cantVendida;
+}
+
+void modificar_datos_de_un_producto(HashMap *mapaProducto,
+                                    HashMap *mapaSemanal) {
+  int opcion, nuevoValor;
+  char nombreProducto[100];
+
+  printf("Ingrese el nombre del producto a modificar\n");
+  scanf("%s", nombreProducto);
+  Pair *buscado = searchMap(mapaProducto, nombreProducto);
+  if (buscado == NULL) {
+    printf("El producto no existe");
+    return;
+  }
+  tipoProducto *producto = buscado->value;
+  tipoProducto *productoSemana = searchMap(mapaSemanal, nombreProducto)->value;
+  printf("Seleccione que quiere cambia\n");
+  printf("1. Precio de compra\n");
+  printf("2. Precio de venta\n");
+  printf("3. Stock inicial\n");
+  scanf("%d", &opcion);
+  getchar();
+  switch (opcion) {
+  case 1:
+    printf("Ingrese el nuevo precio de compra\n");
+    scanf("%d", &nuevoValor);
+    getchar();
+    producto->precioCompra = nuevoValor;
+    productoSemana->precioCompra = nuevoValor;
+    break;
+  case 2:
+    printf("Ingrese el nuevo precio de venta\n");
+    scanf("%d", &nuevoValor);
+    producto->precioVenta = nuevoValor;
+    productoSemana->precioVenta = nuevoValor;
+
+    break;
+  case 3:
+    printf("Ingrese el nuevo stock inicial\n");
+    scanf("%d", &nuevoValor);
+    producto->stockInicial = nuevoValor;
+    productoSemana->stockInicial = nuevoValor;
+  default:
+    printf("Opcion invalida\n");
+    return;
+  }
+}
+
+void Cargar_csv_de_stock(HashMap *mapaProducto, HashMap *mapaSemanal,
+                         char *nombre_archivo, tipoDia *dia) {
+  char caracter[100], *nombre;
+  FILE *archivoCsv = fopen(nombre_archivo, "r");
+  int precioCompra, precioVenta, stockInicial, cantVendida, opcion;
+
+  if (archivoCsv == NULL) {
+    printf("\n—————————————————————————————————————————————————————————————————"
+           "—————————\n");
+    printf(
+        "El archivo %s no existe en el directorio actual o esta mal escrito.\n",
+        nombre_archivo);
+    printf("———————————————————————————————————————————————————————————————————"
+           "———————\n\n");
+    return;
+  }
+
+  fgets(caracter, 99, archivoCsv);
+
+  while (fscanf(archivoCsv, "%m[^,],%d,%d,%d\n", &nombre, &precioCompra,
+                &precioVenta, &stockInicial) != EOF) {
+    tipoProducto *producto = malloc(sizeof(tipoProducto));
+    producto->nombre = strdup(nombre);
+    producto->precioCompra = precioCompra;
+    producto->precioVenta = precioVenta;
+    producto->stockInicial = stockInicial;
+    producto->cantVendida = 0;
+    dia->gastoDia += stockInicial * precioCompra;
+    insertMap(mapaProducto, producto->nombre, producto);
+    if (searchMap(mapaSemanal, producto->nombre) == NULL)
+      insertMap(mapaSemanal, producto->nombre, producto);
+    else {
+      tipoProducto *productoExistente =
+          searchMap(mapaSemanal, producto->nombre)->value;
+      productoExistente->stockInicial += stockInicial;
+    }
+  }
+  printf("\nArchivo importado.\n");
+  fclose(archivoCsv);
+}
+
+void mostrarMapa(HashMap *mapaProducto) {
+  printf("Productos almacenados:\n");
+
+  Pair *pair = firstMap(mapaProducto);
+  while (pair != NULL) {
+    tipoProducto *producto = (tipoProducto *)pair->value;
+    printf("Nombre: %s\n", producto->nombre);
+    printf("Precio de compra: %d\n", producto->precioCompra);
+    printf("Precio de venta: %d\n", producto->precioVenta);
+    printf("Stock inicial: %d\n", producto->stockInicial);
+    printf("Cantidad vendida: %d\n", producto->cantVendida);
+    printf("------------------------\n");
+    pair = nextMap(mapaProducto);
+  }
+}
+
+void Exportar_mapa(char *nombre_archivoDinero, char *nombre_archivo, Heap *monticuloMaximo, Heap *monticuloMinimo, List* listaDia) {
+  int contador = 1;
+  tipoDia *dineroDia = lastList(listaDia);
+  FILE *archivoDinero = fopen(nombre_archivoDinero, "w");
+  fprintf(archivoDinero,"Ganancia,Gastos\n");
+  fprintf(archivoDinero, "%d,%d\n", dineroDia->gananciaDia, dineroDia->gastoDia);
+  fclose(archivoDinero);
+  
+  FILE *archivo = fopen(nombre_archivo, "w");
+  fprintf(archivo,"Producto,Precio de Compra,Precio de Venta,Stock,CantVendida\n");
+
+  tipoProducto *productoMax = heap_top(monticuloMaximo);
+  tipoProducto *productoMin = heap_top(monticuloMinimo);
+  while (contador <= 5) {
+    fprintf(archivo, "%s,%d,%d,%d,%d", productoMax->nombre,
+            productoMax->precioCompra, productoMax->precioVenta,
+            productoMax->stockInicial, productoMax->cantVendida);
+    productoMax->stockInicial=productoMax->stockInicial-productoMax->cantVendida;
+    productoMax->cantVendida=0;
+    fprintf(archivo, "\n");
+    heap_popMax(monticuloMaximo);
+    productoMax = heap_top(monticuloMaximo);
+    contador++;
+  }
+  while (contador <= 10) {
+    fprintf(archivo, "%s,%d,%d,%d,%d", productoMin->nombre,
+            productoMin->precioCompra, productoMin->precioVenta,
+            productoMin->stockInicial, productoMin->cantVendida);
+    productoMin->stockInicial=productoMin->stockInicial-productoMin->cantVendida;
+    productoMin->cantVendida=0;
+    fprintf(archivo, "\n");
+    heap_pop(monticuloMinimo);
+    productoMin = heap_top(monticuloMinimo);
+    contador++;
+  }
+  
+  printf("\nArchivo exportado.\n");
+  printf("————————————————————————————————————————————————————————————\n\n");
+  fclose(archivo);
+}
+
+void Exportar_mapa_semanal(char *nombre_archivo, Heap *monticuloMaximo, Heap *monticuloMinimo, List* listaDia) {
+  int contador = 1;
+  FILE *archivo = fopen(nombre_archivo, "w");
+  fprintf(archivo,
+          "Producto,Precio de Compra,Precio de Venta,Stock,CantVendida,GastoDia,GananciaDia\n");
+
+  tipoProducto *productoMax = heap_top(monticuloMaximo);
+  tipoProducto *productoMin = heap_top(monticuloMinimo);
+  tipoDia *auxiliar = firstList(listaDia);
+  while (contador <= 5) {
+    fprintf(archivo, "%s,%d,%d,%d,%d,%d,%d", productoMax->nombre,productoMax->precioCompra, productoMax->precioVenta, productoMax->stockInicial, productoMax->cantVendida,auxiliar->gastoDia,auxiliar->gananciaDia);
+
+    fprintf(archivo, "\n");
+    productoMax->stockInicial =
+        productoMax->stockInicial - productoMax->cantVendida;
+    productoMax->cantVendida = 0;
+    heap_pop(monticuloMaximo);
+    productoMax = heap_top(monticuloMaximo);
+    contador++;
+    auxiliar = nextList(listaDia);
+  }
+  while(contador <= 10)
+    {
+      if(contador <=7)
+      {
+        fprintf(archivo, "%s,%d,%d,%d,%d,%d,%d\n", productoMin->nombre,productoMin->precioCompra, productoMin->precioVenta, productoMin->stockInicial, productoMin->cantVendida,auxiliar->gastoDia,auxiliar->gananciaDia);
+        auxiliar = nextList(listaDia);
+
+      }
+      else
+      {
+        fprintf(archivo, "%s,%d,%d,%d,%d\n", productoMin->nombre,productoMin->precioCompra, productoMin->precioVenta, productoMin->stockInicial, productoMin->cantVendida);
+      }
+      contador++;
+    }
+
+  printf("\nArchivo exportado.\n");
+  printf("————————————————————————————————————————————————————————————\n\n");
+  fclose(archivo);  
+  system("python pythonSemanal.py");
+
+}
+
+void finalizarDia(HashMap *mapaProducto, HashMap *mapaSemanal, List *listaDia, int *contadorDia, tipoDia *dia) {
+  Heap *monticuloMaximo = createHeap(), *monticuloMinimo = createHeap();
+  
+  pushBack(listaDia, dia);
+  
+  if (*contadorDia != 7) {
+    Pair *aux = firstMap(mapaProducto);
+    while (aux != NULL) {
+      tipoProducto *producto = (tipoProducto *)aux->value;
+      heap_pushMax(monticuloMaximo, producto, producto->cantVendida);
+      heap_pushMin(monticuloMinimo, producto, producto->cantVendida);
+      aux = nextMap(mapaProducto);
+    }
+    char *nombre_archivo = "csvDiario.csv";
+    char *nombre_archivoDinero = "csvDiarioDinero.csv";
+    Exportar_mapa(nombre_archivoDinero,nombre_archivo, monticuloMaximo, monticuloMinimo, listaDia);
+    free(monticuloMaximo);
+    free(monticuloMinimo);
+    //system("python pythonDiarioMax.py");
+    //system("python pythonDiarioMin.py");
+    system("python pythonDiarioGanancia.py");
+  } else {
+    Pair *aux = firstMap(mapaSemanal);
+    char *nombre_archivo;
+    while (aux != NULL) {
+      tipoProducto *producto = (tipoProducto *)aux->value;
+      heap_pushMax(monticuloMaximo, producto, producto->cantVendida);
+      heap_pushMin(monticuloMinimo, producto, producto->cantVendida);
+      aux = nextMap(mapaSemanal);
+    }
+    printf("¿Como quiere llamar el archivo de esta semana?\n");
+    scanf("%m[^\n]", &nombre_archivo);
+    getchar();
+    Exportar_mapa_semanal(nombre_archivo, monticuloMaximo, monticuloMinimo, listaDia);
+    exit(EXIT_SUCCESS);
+    }
+}
+
+int main() {
+  tipoDia *dia = malloc(sizeof(tipoDia));
+  dia->gananciaDia = 0;
+  dia->gastoDia = 0;
+  HashMap *mapaProducto = createMap((long)100);
+  HashMap *mapaSemanal = createMap((long)100);
+  List *listaDias = createList();
+  char caracter[100], *nombre, *archivoCargado;
+  int precioCompra, precioVenta, stockInicial, opcion, contadorDia = 1;
+  FILE *ArchivoPrueba = fopen("ArchivoPrueba.csv", "r");
+  fgets(caracter, 99, ArchivoPrueba);
+  while (fscanf(ArchivoPrueba, "%m[^,],%d,%d,%d\n", &nombre, &precioCompra,
+                &precioVenta, &stockInicial) != EOF) {
+    tipoProducto *producto = malloc(sizeof(tipoProducto));
+    producto->nombre = strdup(nombre);
+    producto->precioCompra = precioCompra;
+    producto->precioVenta = precioVenta;
+    producto->stockInicial = stockInicial;
+    producto->cantVendida = 0;
+    insertMap(mapaProducto, producto->nombre, producto);
+    tipoProducto *copiaProductoAux = malloc(sizeof(tipoProducto));
+    copiaProductoAux->nombre = strdup(nombre);
+    copiaProductoAux->precioCompra = precioCompra;
+    copiaProductoAux->precioVenta = precioVenta;
+    copiaProductoAux->stockInicial = stockInicial;
+    copiaProductoAux->cantVendida = 0;
+    dia->gastoDia += stockInicial * precioCompra;
+    insertMap(mapaProducto, producto->nombre, producto);
+    insertMap(mapaSemanal, producto->nombre, copiaProductoAux);
+  }
+
+  fclose(ArchivoPrueba);
+
+  while (true) {
+    printf("\n--- Menú Principal ---\n");
+    printf("1. Registro de productos\n");
+    printf("2. Control de stock\n");
+    printf("3. Modificar datos de un producto\n");
+    printf("4. Comparar y graficar dos informes semanales\n");
+    printf("5. Cargar csv de stock\n");
+    printf("6. Finalizar el día, creación de gráfico e informe\n");
+    printf("Seleccione una opción: \n");
+    scanf("%d", &opcion);
+    getchar();
+    switch (opcion) {
+    case 1:
+      registro_de_productos(mapaProducto, mapaSemanal, dia);
+      break;
+    case 2:
+      Control_de_stock(mapaProducto, mapaSemanal, dia);
+      break;
+    case 3:
+      modificar_datos_de_un_producto(mapaProducto, mapaSemanal);
+      break;
+    case 4:
+      mostrarMapa(mapaProducto);
+      break;
+    case 5:
+      printf(
+          "\n————————————————————————————————————————————————————————————\n");
+      printf("Ingrese el nombre del archivo, introduzca el formato (.csv)\n");
+      scanf("%m[^\n]", &archivoCargado);
+      getchar();
+      if (strstr(archivoCargado, ".csv") == 0) {
+        printf("\nEl formato del archivo '%s' es incorrecto.\n",
+               archivoCargado);
+        printf(
+            "————————————————————————————————————————————————————————————\n\n");
+        break;
+      }
+      Cargar_csv_de_stock(mapaProducto, mapaSemanal, archivoCargado, dia);
+      printf(
+          "————————————————————————————————————————————————————————————\n\n");
+      break;
+    case 6:
+      finalizarDia(mapaProducto, mapaSemanal, listaDias, &contadorDia, dia);
+      contadorDia++;
+      break;
+    default:
+      break;
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # proyecto-v3
+Grupo overfore.
+
+Integrantes: 
+  - Gabriel Cuibin
+  - Patricio Figueroa
+  - Marcelo Flores
+  - Lucas Zamora
+.
+Como compilar:
+
+ Se puede ejecutar desde este replit sin necesidad de insertar archivos en el replit o tener un compilador de c en el computador.
+
+ Este código funciona de forma semanal, es decir, luego de cada semana, el código se cierra y se tiene que apretar el botón run de replit para poder usarlo otra semana, además, se necesita tener un csv llamado ArchivoPrueba.csv, el cuál indica los productos con los que se inicia la ejecución del código y deben seguir los formatos ya establecidos para el correcto funcionamiento del código, también se necesitan los archivos .c y .h, los cuales corresponden al main y TDAS usados en la aplicación. Con la función cargar csv,si un dato en el csv ya existe en la base de datos anterior, se sobrescribe el stock y se suman las ganancias en función de la cantidad de items comprados, sucede lo mismo con modificar datos de un producto. En la función 6, se crea un csv residual llamado csvDiario.csv, el cuál se sobreescribe y reemplaza por otro en función del día, además se utilizan códigos en python que leen dichos csv y muestran los gráficos, en el gráfico de productos menos vendidos, los productos sin ventas son considerados mínimos, por lo que no hay un orden entre lo mostrado y los items siempre que tengan la misma cantidad de vendidos. Cada vez que se crea un gráfico, se guardan copias png diarias de los gráficos llamados graficoDiarioMin y graficoDiarioMax y de todos los gráficos creados en el código, estos cambian todos los días. Cuando se llama a la función 6 siete veces, se considera que ya ocurrió una semana, por lo que se muestran cuatro gráficos, grafico de los 5 productos más vendidos y menos vendidos de la semana, gráfico de ganancia (es decir, total ganado menos total gastado) y de gasto en función del día de la semana. Un archivo temporal es el nombre.csv, es un archivo en el cual se traspasa el nombre del informe semanal que crea el usuario a la función de python, por lo que se genera de forma automática y sirve para no preguntar dos veces por el mismo nombre al usuario.
+
+╔══════════════════╦═══════════════╦══════════════════╦══════════════╦════════════════════╦═══════════════════════════════╗
+║    Nombre        ║ Participación ║  Responsabilidad ║ Comunicacion ║ Calidad de Trabajo ║ Trabajo en Equipo ║ Pje Total ║
+╠══════════════════╬═══════════════╬══════════════════╬══════════════╬════════════════════╬═══════════════════════════════╣
+║  Gabriel Cuibin  ║      20       ║        20        ║      20      ║         20         ║          20       ║    100    ║
+╠══════════════════╬═══════════════╬══════════════════╬══════════════╬════════════════════╬═══════════════════════════════╣
+║  Lucas Zamora    ║      20       ║        20        ║      20      ║         20         ║          20       ║    100    ║
+╠══════════════════╬═══════════════╬══════════════════╬══════════════╬════════════════════╬═══════════════════════════════╣
+║  Marcelo Flores  ║      20       ║        20        ║      20      ║         20         ║          20       ║    100    ║
+╠══════════════════╬═══════════════╬══════════════════╬══════════════╬════════════════════╬═══════════════════════════════╣
+║Patricio Figueroa ║      20       ║        20        ║      20      ║         20         ║          20       ║    100    ║
+╚══════════════════╩═══════════════╩══════════════════╩══════════════╩════════════════════╩═══════════════════════════════╝
+
+Nuestra capacidad colaborativa como equipo se consolida a través de la plataforma Repl.it y visual studio, donde ejecutamos nuestras tareas de manera altamente eficiente. Compartimos un objetivo común y trabajamos de forma conjunta para lograrlo. Mantenemos una comunicación fluida y clara, lo que nos permite mantenernos en sintonía en todo momento. Cada miembro del equipo aporta habilidades y conocimientos únicos que se complementan entre sí, lo que nos capacita para abordar con éxito cualquier tarea. La confianza y el respeto son valores esenciales en nuestra dinámica de trabajo, lo que nos permite trabajar con eficacia y obtener resultados de alta calidad.
+
+Gabriel Cuibin: Su contribución fue destacada, demostrando habilidad para identificar ágilmente errores en el código y encontrar soluciones efectivas a los problemas planteados. Además, muestra una notable capacidad para comprender y ejecutar las ideas propuestas por el grupo.
+
+Lucas Zamora: Su responsabilidad en el trabajo fue sobresaliente, manteniendo una actitud seria y comprometida en todo momento. Desde el inicio del proyecto, se esforzó por comprender claramente sus responsabilidades y objetivos, trabajando diligentemente para cumplirlos dentro de los plazos establecidos.
+
+Marcelo Flores: La comunicación dentro del grupo resultó efectiva, ya que los miembros lograron comprender las ideas y se brindaron explicaciones mutuas cuando surgieron confusiones. Este miembro se destacó por su habilidad para mediar entre los demás en situaciones de desacuerdo. Gracias a esta comunicación fluida, el trabajo en equipo se volvió más eficiente y se lograron alcanzar los objetivos de manera efectiva.
+
+Patricio Figueroa: Participó activamente y de manera altamente productiva en el trabajo. En todo momento aportó ideas para resolver los problemas que surgieron, lo que nos ayudó a encontrar soluciones originales y efectivas. Además, mantuvo una buena comunicación con el equipo y siempre colaboró para lograr los objetivos del proyecto en el tiempo acordado.
